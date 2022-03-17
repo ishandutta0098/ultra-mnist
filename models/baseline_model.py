d67dd98795a6cb62276b10c597507d110ee676e0
ishandutta0098 <ishandutta0098@gmail.com>
2022-03-17 10:30:27 +0530

    prepare baseline model

---
+++ models/baseline_model.py
@@ -0,0 +1,78 @@
+import torch.nn as nn 
+import timm 
+
+class ResNetModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.model = timm.create_model(
+            cfg['MODEL']['MODEL_NAME'], 
+            pretrained=cfg['MODEL']['PRETRAINED']
+            )
+        
+        self.model.fc = nn.Linear(self.model.fc.in_features, 128)
+        self.dropout = nn.Dropout(0.1)
+        self.out = nn.Linear(128, cfg['MODEL']['NUM_CLASSES'])
+
+    def forward(self, image):
+        x = self.model(image)
+        x = self.dropout(x)
+        output = self.out(x)
+        return output
+
+class ResNextModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.model = timm.create_model(
+            cfg['MODEL']['MODEL_NAME'], 
+            pretrained=cfg['MODEL']['PRETRAINED']
+            )
+        
+        self.model.fc = nn.Linear(self.model.fc.in_features, 128)
+        self.dropout = nn.Dropout(0.3)
+        self.out = nn.Linear(128, cfg['MODEL']['NUM_CLASSES'])
+
+    def forward(self, image):
+        x = self.model(image)
+        x = self.dropout(x)
+        output = self.out(x)
+        return output
+
+class VGGModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.model = timm.create_model(
+            cfg['MODEL']['MODEL_NAME'], 
+            pretrained=cfg['MODEL']['PRETRAINED']
+            )
+        
+        self.model.head.fc = nn.Linear(self.model.head.fc.in_features, 128)
+        self.dropout = nn.Dropout(0.1)
+        self.out = nn.Linear(128, cfg['MODEL']['NUM_CLASSES'])
+
+    def forward(self, image):
+        x = self.model(image)
+        x = self.dropout(x)
+        output = self.out(x)
+        return output
+
+class ViTModel(nn.Module):
+    def __init__(self, cfg):
+
+        super(ViTModel, self).__init__()
+
+        self.model = timm.create_model(
+            cfg['MODEL']['MODEL_NAME'], 
+            pretrained=cfg['MODEL']['PRETRAINED']
+            )
+
+        self.model.head = nn.Linear(
+            self.model.head.in_features, 
+            cfg['MODEL']['NUM_CLASSES']
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
