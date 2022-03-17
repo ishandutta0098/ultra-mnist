fa471eb065d9253cc51485a27af46152f544aba1
ishandutta0098 <ishandutta0098@gmail.com>
2022-03-17 10:34:23 +0530

    create main script

---
+++ main.py
@@ -0,0 +1,92 @@
+import os
+from re import M
+import pandas as pd
+import torch
+import argparse
+import wandb 
+from tqdm.auto import tqdm
+
+import utilities.utils as utils
+import training.train as train 
+import utilities.model_utils as model_utils
+
+if __name__ == "__main__":
+
+    # Login to weights and biases
+    wandb.login()
+
+    # Argument Parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, required=True)
+    parser.add_argument('--device', type=str, required=True, help='cuda or cpu')
+    parser.add_argument('--environ', type=str, required=True, help='colab or local or ec2')
+    parser.add_argument('--check', type=bool)
+
+    args = parser.parse_args()
+
+    # Load Configuration File
+    cfg = utils.load_config(args.config)
+    
+    if args.environ == 'colab':
+        # mount drive at drive.mount('/drive)
+        BASE_PATH = cfg['DATA']['COLAB_BASE_PATH']
+
+    elif args.environ == 'local':
+        BASE_PATH = cfg['DATA']['BASE_PATH']
+
+    elif args.environ == 'ec2':
+        BASE_PATH = cfg['DATA']['BASE_PATH']
+
+    # Get paths
+    TRAIN_PATH = BASE_PATH + cfg['DATA']['TRAIN_CSV']
+    VALID_PATH = BASE_PATH + cfg['DATA']['TRAIN_CSV']
+
+    PRED_PATH = BASE_PATH + cfg['PREDICT']['PRED_CSV']
+    
+    # Device
+    DEVICE = torch.device(args.device)
+
+    # Make model
+    model, optimizer, scheduler = model_utils.make_model(cfg)
+    model.to(DEVICE)
+
+    # Load data
+    df_train = pd.read_csv(TRAIN_PATH)
+    df_valid = pd.read_csv(VALID_PATH)
+
+    # Get Class weights for calculating Loss
+    if cfg['TRAIN']['WEIGHTS'] == True:
+        weights = utils.get_class_weights(df_train)
+
+    else: 
+        weights = None
+
+    if args.check == True:
+        df_train = df_train.head(1000)
+
+    # Train and validate model
+
+    print("\n---------------------------------------------------------")
+    print(f"########## Initialize Training ##########")
+    print("---------------------------------------------------------")
+    print()
+
+    train.run_training(
+        args,
+        cfg,
+        model, 
+        optimizer, 
+        scheduler,
+        weights = weights,
+        device=DEVICE,
+        num_epochs=cfg['TRAIN']['EPOCHS'],
+        df_train = df_train,
+        df_valid = df_valid,
+        pred_csv=PRED_PATH,
+        base_path = BASE_PATH    
+    )
+
+    print("\n---------------------------------------------------------")
+    print(f"########## Training Completed ##########")
+    print("---------------------------------------------------------")
+    print()
